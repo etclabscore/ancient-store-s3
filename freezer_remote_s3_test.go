84a453e22963f3db6fd57418ed3233a8df391c22
meows <b5c6@protonmail.com>
2020-07-31 12:59:15 -0500

    main: install configurable s3 bucket namespacing
    See comments; bucket namespacing will be necessary
    since s3 bucket names must be globally unique.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- freezer_remote_s3_test.go
+++ freezer_remote_s3_test.go
@@ -7,11 +7,26 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"testing"
 )
 
+var (
+	// s3BucketNamespace is used to unique-ify S3 bucket names.
+	// s3 bucket names must be unique GLOBALLY.
+	// This can be configured by the tester with the environment variable: AWS_BUCKET_NAMESPACE.
+	// If this environment variable is not set, the OS hostname will be used.
+	// Ye be warned.
+	// Buckets are named with the following pattern:
+	// ancientstore-<namespace>-<testname>
+	//
+	// Only the <namespace> value is user-configurable.
+	s3BucketNamespace = ""
+)
+
 func init() {
+	// AWS credential defaults.
 	if os.Getenv("AWS_REGION") == "" {
 		log.Println("Setting default AWS_REGION credential")
 		os.Setenv("AWS_REGION", "us-west-1")
@@ -19,6 +34,16 @@ func init() {
 	if os.Getenv("AWS_PROFILE") == "" {
 		log.Println("Setting default AWS_PROFILE credential")
 		os.Setenv("AWS_PROFILE", "developers-s3")
+	}
+
+	// S3 bucket naming defaults.
+	s3BucketNamespace = os.Getenv("AWS_BUCKET_NAMESPACE")
+	if s3BucketNamespace == "" {
+		hostname, err := os.Hostname()
+		if err != nil {
+			panic(err)
+		}
+		s3BucketNamespace = hostname
 	}
 }
 
@@ -47,12 +72,9 @@ func runTestCase(testCase string, t *testing.T) {
 	defer os.RemoveAll(tmpDir)
 	ipcPath := filepath.Join(tmpDir, "ancient.ipc")
 	go func() {
-		// https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html
-		bucketName := fmt.Sprintf("ancientstore-%s", testCase)
-		bucketName = strings.ReplaceAll(bucketName, "_", "")
-		bucketName = strings.ToLower(bucketName)
-		if len(bucketName) > 63 {
-			bucketName = bucketName[:63]
+		bucketName, err := bucketNameForTest(testCase)
+		if err != nil {
+			log.Fatalln("failed to create bucket name:", err)
 		}
 		if err := app.Run([]string{"ancient-store-s3", "--bucket", bucketName, "--loglevel", "3", "--ipcpath", ipcPath}); err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -88,4 +110,19 @@ func TestIntegration(t *testing.T) {
 			runTestCase(testCase, t)
 		})
 	}
+}
+
+func bucketNameForTest(testName string) (string, error) {
+	// https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html
+	//
+	// These sanitizations ARE NOT COMPLETE; it is still possible for users to fuck this up.
+	// But this is about the extent of my patience.
+	bucketName := fmt.Sprintf("ancientstore-%s-%s", s3BucketNamespace, testName)
+	bucketName = strings.ToLower(bucketName)
+	re := regexp.MustCompile(`[^a-zA-Z.-]`)
+	bucketName = re.ReplaceAllString(bucketName, "")
+	if len(bucketName) > 63 {
+		bucketName = bucketName[:63]
+	}
+	return bucketName, nil
 }
