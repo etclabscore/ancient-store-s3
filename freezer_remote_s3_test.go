746cb77d6ffadc69504166f282074c123aa89fea
Zane Starr <zcstarr@gmail.com>
2020-07-30 23:11:45 -0700

    go.mod, go.sum, freezer_remote_s3_test.go: sketch of what test harness might look like

--- freezer_remote_s3_test.go
+++ freezer_remote_s3_test.go
@@ -1,104 +1,66 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"log"
-	"math/big"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
 	"testing"
-
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/rlp"
-	lru "github.com/hashicorp/golang-lru"
 )
 
-func TestCacheBatches(t *testing.T) {
-	c, _ := lru.New(32)
-	for i := 0; i < 10; i++ {
-		c.Add(uint64(i), "anything")
-	}
-	batches := cacheKeyGroups(c, 2)
-	if len(batches) != 5 {
-		t.Fatalf("bad %d", len(batches))
-	}
+var (
+	ipcPath   = os.TempDir() + "ancient.ipc"
+)
+
+func runMain(bucket string) {
+	os.Setenv("AWS_REGION","us-west-1")
+	os.Setenv("AWS_PROFILE", "developers-s3")
+
+	os.Args = append([]string{"./ancient-store-s3", "--bucket", fmt.Sprintf("etclabs-unit-test-%v", rand.Int()), "--loglevel", "3", "--ipcpath", ipcPath})
+	main()
+
 }
 
-func TestDifficultyRLPDecoding(t *testing.T) {
-	td := big.NewInt(42)
-	b, err := rlp.EncodeToBytes(td)
+func getTestCases() []string {
+	testCmd := exec.Command("go", "test", "github.com/ethereum/go-ethereum/core", "-list", "_RemoteFreezer")
+	testCmd.Stderr = os.Stderr
+	output, err := testCmd.Output()
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
-	difficulty := new(big.Int)
-	err = rlp.DecodeBytes(b, difficulty)
+	cases := strings.Split(string(output), "\n")
+	parsedCases := []string{}
+	for _, c := range cases {
+		if strings.HasPrefix(c, "Test") {
+			parsedCases = append(parsedCases, c)
+		}
+	}
+	return parsedCases
+}
+
+func runTestCase(testCase string) {
+
+	go func() {
+		runMain(testCase)
+	}()
+	testFilter := fmt.Sprintf("-run=%s", testCase)
+	fmt.Println("TESTCASE===================:", testCase)
+	testCmd := exec.Command("go", "test", "github.com/ethereum/go-ethereum/core", "-count=1", "-v", testFilter)
+	testCmd.Env = os.Environ()
+	testCmd.Env = append(os.Environ(), fmt.Sprintf("GETH_ANCIENT_RPC=%s", ipcPath))
+	testCmd.Stderr = os.Stderr
+	//	testCmd.Stdout = os.Stdout
+	err := testCmd.Run()
 	if err != nil {
-		t.Error(err)
+		panic(err)
 	}
-	t.Log(difficulty)
 }
 
-func TestHashRLPCodec(t *testing.T) {
-	h1 := common.HexToHash("0xbadface")
-	b, err := rlp.EncodeToBytes(h1)
-	if err != nil {
-		t.Fatal(err)
+func TestIntegration(t *testing.T) {
+
+	testCases := getTestCases()
+	for _, testCase := range testCases {
+		runTestCase(testCase)
 	}
-	h2 := common.Hash{}
-	err = rlp.DecodeBytes(b, &h2)
-	if err != nil {
-		t.Error(err)
-	}
-	log.Println(h2.Hex())
-}
-
-func TestBlockKeying(t *testing.T) {
-	m := 32
-	n := 17
-	mod := n % m
-	div := n / m
-	t.Log(mod)
-	t.Log(div)
-	t.Log(n*div + mod)
-}
-
-func TestIndexThing(t *testing.T) {
-	a := []int{1, 2, 3}
-	t.Log(a[:0])
-}
-
-func TestSpliceBackwards(t *testing.T) {
-	a := []int{1, 2, 3, 4, 5}
-	mod := 3
-	rem := len(a) % mod
-	t.Log(a[len(a)-rem:])
-}
-
-func TestSlice(t *testing.T) {
-	a := []int{1, 2, 3, 4, 5}
-	t.Log(a[2:5])
-}
-
-func Test30kHashes(t *testing.T) {
-	n := 32 * 32 * 32
-	hashes := make([]common.Hash, n)
-	for i := 0; i < n; i++ {
-		hashes[i] = common.HexToHash("0xbadface")
-	}
-	b, err := json.Marshal(hashes)
-	if err != nil {
-		t.Fatal(err)
-	}
-	c := bytes.Count(b, []byte(""))
-	t.Log(c)
-}
-
-func TestCache_TruncateFrom(t *testing.T) {
-	/*c := newCache()
-	for i := 0; i < 32; i++ {
-		c.add(uint64(i), fmt.Sprintf("n%d", i))
-	}
-	c.truncateFrom(32)
-	b := c.batch(0, 36)
-	t.Log(c.sl, b)
-	*/
 }
--- go.mod
+++ go.mod
@@ -11,4 +11,4 @@ require (
 	gopkg.in/urfave/cli.v1 v1.20.0
 )
 
-replace github.com/ethereum/go-ethereum => github.com/etclabscore/core-geth v1.11.10-0.20200730130117-dc98713fac98
+replace github.com/ethereum/go-ethereum => github.com/etclabscore/core-geth v1.11.10-0.20200730202736-685132e050e9
